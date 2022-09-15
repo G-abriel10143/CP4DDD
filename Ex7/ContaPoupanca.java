5be76e2fec50613b2a885df8b0fd4e6b40cce2dc
Gabriel Cerqueira Alves <101301397+Gabriel0030@users.noreply.github.com>
2022-09-15 20:12:21 -0300

    Create ContaPoupanca.java

---
+++ Ex7/ContaPoupanca.java
@@ -0,0 +1,27 @@
+package ex7;
+
+public class ContaPoupanca extends Conta implements continha {
+
+    private double rentabilidade;
+
+    public double getRentabilidade() {
+        return rentabilidade;
+    }
+
+    public void setRentabilidade(double rentabilidade) {
+        this.rentabilidade = rentabilidade;
+    }
+
+    public ContaPoupanca(double rentabilidade) {
+        this.rentabilidade = rentabilidade;
+    }
+
+    public ContaPoupanca() {
+    }
+
+	@Override
+	public void depositar(double valor) {
+		// TODO Auto-generated method stub
+		
+	}
+}
