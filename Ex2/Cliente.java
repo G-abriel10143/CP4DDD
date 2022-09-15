fafc4bf584e600044f5ae92754c75dfebfd35fb7
Gabriel Cerqueira Alves <101301397+Gabriel0030@users.noreply.github.com>
2022-09-15 16:21:33 -0300

    Create Cliente.java

---
+++ Ex2/Cliente.java
@@ -0,0 +1,28 @@
+package ex2;
+
+public class Cliente {
+	
+	private int id;
+	private String cliente;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return cliente;
+	}
+	public void setName(String cliente) {
+		this.cliente = cliente;
+	}
+	
+	
+	public cliente() {
+		
+	}
+ 	
+
+}
