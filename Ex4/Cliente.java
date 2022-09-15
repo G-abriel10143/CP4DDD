45b11b67f36d34570529dad1d8a16a3a2e2597f6
Gabriel Cerqueira Alves <101301397+Gabriel0030@users.noreply.github.com>
2022-09-15 19:40:09 -0300

    Create Cliente.java

---
+++ Ex4/Cliente.java
@@ -0,0 +1,28 @@
+package ex4;
+
+public class Cliente {
+	
+	private int id;
+	private String name;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public Cliente() {
+		
+	}
+ 	
+
+}
