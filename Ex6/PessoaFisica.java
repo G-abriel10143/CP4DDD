e6ad99540445fe309f91503608c0f63ace41b0a9
Gabriel Cerqueira Alves <101301397+Gabriel0030@users.noreply.github.com>
2022-09-15 20:07:14 -0300

    Create PessoaFisica.java

---
+++ Ex6/PessoaFisica.java
@@ -0,0 +1,19 @@
+package ex6;
+
+public class PessoaFisica extends Cliente {
+
+	private int cpf;
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	
+	public PessoaFisica() {
+		
+	}
+	
+}
