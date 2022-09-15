50557e26c9d774e9cd740b24669a1bc40db0b7d2
Gabriel Cerqueira Alves <101301397+Gabriel0030@users.noreply.github.com>
2022-09-15 20:07:54 -0300

    Create PessoaJuridica.java

---
+++ Ex6/PessoaJuridica.java
@@ -0,0 +1,20 @@
+package ex6;
+
+public class PessoaJuridica extends  Cliente {
+	
+	private int cnpj;
+
+	public int getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(int cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	
+	public PessoaJuridica() {
+		
+	}
+
+}
