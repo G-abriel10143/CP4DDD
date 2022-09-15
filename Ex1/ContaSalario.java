8ff770792333f8747b698d01b366a0cffa0304c6
Gabriel Cerqueira Alves <101301397+Gabriel0030@users.noreply.github.com>
2022-09-15 12:53:52 -0300

    Create ContaSalario.java

---
+++ Ex1/ContaSalario.java
@@ -0,0 +1,19 @@
+package Ex1;
+
+public class ContaSalario extends Conta {
+
+	private int cpfEmpregador;
+
+	public int getcpfEmpregador() {
+		return cpfEmpregador;
+	}
+
+	public void setcpfEmpregador(int cpfEmpregador) {
+		this.cpfEmpregador = cpfEmpregador;
+	}
+
+	public ContaSalario() {
+
+	}
+
+}
